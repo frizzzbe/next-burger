8182b79e5bb9ee39b225a27894d3130b65e75ab8
Arkadiy Minasyan <a.minasyan@notamedia.com.ru>
2023-11-03 12:12:34 +0300

    burgers page

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
-const Burgers = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:5000/items');
+  const data = await res.json();
+
+  return {
+    props: { burgers: data }
+  }
+}
+
+const Burgers = ({burgers}) => {
   return (
-    <div>
-      <h1>Наши бургеры</h1>
-    </div>
+    <>
+      <Head>
+        <title>Наши бургеры</title>
+      </Head>
+      <div>
+        <h1>Наши бургеры</h1>
+        {burgers.map(burger => (
+          <Link href={`/burgers/${burger.id}`} key={burger.id} className="burgerCard">
+            <div className="imageContainer">
+              <Image 
+                src={`${burger.image}`} 
+                alt={`${burger.name}`} 
+                width={150}
+                height={150}
+                layout="responsive" 
+                objectFit="cover"
+              />
+            </div>
+            <div>
+              <h3>{ burger.name }</h3>
+              <p>{ burger.desc }</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
 
