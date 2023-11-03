f74d599e889c2f2efcd2f6c772da3a4ec9b390c3
Arkadiy Minasyan <a.minasyan@notamedia.com.ru>
2023-11-03 10:05:02 +0300

    init

---
+++ components/header.js
@@ -0,0 +1,12 @@
+import { SiBurerking } from 'react-icons/si'
+
+const Header = () => {
+  return (
+    <div>
+        <SiBurerking/>
+        <h1>Наши бургеры</h1>
+    </div>
+  )
+}
+
+export default Header
