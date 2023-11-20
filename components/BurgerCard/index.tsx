f4bc2dae97c439713a05a36a62ce1f02ca401fd2
Arkadiy Minasyan <a.minasyan@notamedia.com.ru>
2023-11-20 13:52:19 +0300

    Burger Card Component

---
+++ components/BurgerCard/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { burgerType } from "../../types";
+import st from "./Burger.module.css";
+import Link from "next/link";
+
+const BurgerCard = ({ burger }: { burger: burgerType }) => {
+  return (
+    <Link
+      href={`/burgers/${burger.id}`}
+      key={burger.id}
+      className={st.burgerCard}
+    >
+      <div className={st.imageContainer}>
+        <Image
+          src={`${burger.image}`}
+          alt={`${burger.name}`}
+          width={150}
+          height={150}
+        />
+      </div>
+      <div>
+        <h3>{burger.name}</h3>
+        <p>{burger.desc}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default BurgerCard;
--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import { burgersType } from "../../types";
+import BurgerCard from "../../components/BurgerCard";
 
 export const getStaticProps = (async () => {
   const res = await fetch(`${process.env.API_URL}/api/burgers`);
@@ -26,24 +27,7 @@ const Burgers = ({
       <div>
         <h1>Наши бургеры</h1>
         {burgers.map((burger) => (
-          <Link
-            href={`/burgers/${burger.id}`}
-            key={burger.id}
-            className="burgerCard"
-          >
-            <div className="imageContainer">
-              <Image
-                src={`${burger.image}`}
-                alt={`${burger.name}`}
-                width={150}
-                height={150}
-              />
-            </div>
-            <div>
-              <h3>{burger.name}</h3>
-              <p>{burger.desc}</p>
-            </div>
-          </Link>
+          <BurgerCard key={burger.id} burger={burger} />
         ))}
       </div>
     </>
