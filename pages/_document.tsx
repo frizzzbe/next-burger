c7aef734b18bb9305991463f61747e6812d05365
Arkadiy Minasyan <a.minasyan@notamedia.com.ru>
2023-11-21 11:04:34 +0300

    css prettify

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <title>Супер бургеры</title>
+        <meta
+          name="description"
+          content="Супер бургеры по лучшим рецептам от шефа"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,6 @@ const Main: FC = () => {
     <>
       <Head>
         <title>Супер бургеры | Главная</title>
-        <meta
-          name="description"
-          content="Супер бургеры по лучшим рецептам амазонии"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Home />
     </>
