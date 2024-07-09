8ab007ead90bed8d97995a1c1d63208f290f75ec
Arkadiy <frizzzbe@yandex.ru>
2024-07-09 22:49:54 +0300

    active link with pathname

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,42 +1,42 @@
 import Link from "next/link";
 import { SiBurgerking } from "react-icons/si";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import type { FC } from "react";
 import styles from "./Header.module.css";
 import ProfileLink from "../ProfileLink";
 
 const Header: FC = () => {
-	const router = useRouter();
+	const pathname = usePathname();
 
 	return (
 		<header>
 			<SiBurgerking className={styles.mainSvg} />
 			<h1 className={styles.mainTitle}>Наши бургеры</h1>
 			<nav>
-				<Link href="/" className={router.pathname == "/" ? styles.active : ""}>
+				<Link href="/" className={pathname == "/" ? styles.active : ""}>
 					Домой
 				</Link>
 				<Link
 					href="/about"
-					className={router.pathname == "/about" ? styles.active : ""}
+					className={pathname == "/about" ? styles.active : ""}
 				>
 					О нас
 				</Link>
 				<Link
 					href="/reviews"
-					className={router.pathname == "/reviews" ? styles.active : ""}
+					className={pathname == "/reviews" ? styles.active : ""}
 				>
 					Отзывы
 				</Link>
 				<Link
 					href="/users"
-					className={router.pathname.includes("/users") ? styles.active : ""}
+					className={pathname.includes("/users") ? styles.active : ""}
 				>
 					Юзеры
 				</Link>
 				<Link
 					href="/burgers"
-					className={router.pathname.includes("/burgers") ? styles.active : ""}
+					className={pathname.includes("/burgers") ? styles.active : ""}
 				>
 					Бургеры
 				</Link>
