0e86299a58d2318c1fcc7a150ed66893c875d9c2
Arkadiy <frizzzbe@yandex.ru>
2024-10-08 02:38:35 +0300

    optimised images

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,16 +1,15 @@
-import type { FC } from "react"
 import useLocale from "@/hooks/useLocale"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "./Home.module.css"
 
-const Home: FC = () => {
+const Home = () => {
   const i18n = useLocale()
   return (
     <>
       <h1 className={styles.mainPage}>{i18n.main}</h1>
       <div className={styles.mainImage}>
-        <Image src="/fatburger.png" width={300} height={200} alt={i18n.mainTitle} />
+        <Image src="/fatburger.png" width={300} height={200} alt={i18n.mainTitle} priority={true} />
       </div>
       <p className={styles.text}>{i18n.mainP1}</p>
       <p className={styles.text}>{i18n.mainP2}</p>
