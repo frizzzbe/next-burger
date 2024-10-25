ebec9e3508cc5337d281a33aa0c7d390ce5eb9e5
Arkadiy <frizzzbe@yandex.ru>
2024-10-25 16:00:58 +0300

    added dropdown logic to hide menu

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -1,31 +1,34 @@
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef, useEffect, useCallback } from "react"
 import style from "./Dropdown.module.css"
 import { CiUser } from "react-icons/ci"
 import type { DropdownProps } from "./types"
 
 export const Dropdown = ({ children }: DropdownProps) => {
   const container = useRef<HTMLDivElement>()
-  const [dropdownState, setDropdownState] = useState({ open: false })
+  const [dropdownState, setDropdownState] = useState(false)
 
-  const handleDropdownClick = () => setDropdownState({ open: !dropdownState.open })
-
-  const handleClickOutside = (e) => {
+  const handleDropdownClick = () => setDropdownState(!dropdownState)
+  const handleClickOutside = useCallback((e) => {
     if (container.current && !container.current.contains(e.target)) {
-      setDropdownState({ open: false })
+      setDropdownState(false)
     }
-  }
+  }, [])
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
-  }, [])
+    document.addEventListener("click", handleClickOutside)
+    return () => document.removeEventListener("click", handleClickOutside)
+  }, [handleClickOutside])
 
   return (
     <div className={style.container} ref={container}>
       <button type="button" className={style.button} onClick={handleDropdownClick}>
         <CiUser />
       </button>
-      {dropdownState.open && <nav className={style.dropdown}>{children}</nav>}
+      {dropdownState && (
+        <nav className={style.dropdown} onClick={handleDropdownClick}>
+          {children}
+        </nav>
+      )}
     </div>
   )
 }
